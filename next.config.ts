495a3cc35c14fd2d2c8cb27ed922ee8571deec53
zhugez <59059054+zhugez@users.noreply.github.com>
2025-12-03 20:02:46 +0700

    fix: Resolve merge conflicts in configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   /* config options here */
   reactCompiler: true,
-=======
-    /* config options here */
->>>>>>> 275f30c916b53c65ad06e561f948e73d373e6e29
 };
 
 export default nextConfig;
